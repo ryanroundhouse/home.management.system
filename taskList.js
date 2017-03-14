9492885f57231d3e888a8e57cd9e0104e16e1843
Ryan Graham <ryan@graham.pub>
2017-03-13 21:59:50 -0400

    refactored some javascript to be more object oriented.  Also adds a div to each taskcard for each property

---
+++ taskList.js
@@ -0,0 +1,15 @@
+﻿function TaskListManager() {
+}
+
+TaskListManager.prototype.loadTaskListFromString = function (jsonString) {
+    var parsedTaskList = JSON.parse(jsonString);
+    return parsedTaskList.taskList;
+}
+
+TaskListManager.prototype.addTaskPropertiesToElement = function (element, taskProperties) {
+    jQuery.each(taskProperties, function (tIndex, prop) {
+        var innerDiv = document.createElement('div');
+        innerDiv.appendChild(document.createTextNode(prop));
+        element.appendChild(innerDiv);
+    });
+}
